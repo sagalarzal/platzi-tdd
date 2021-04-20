6edab63fc642cd6137d807a20831df109da63d77
Sergio Galarza <sergiogalarzalopez@gmail.com>
2021-04-20 16:36:59 -0500

    commit fin de la prueba

---
+++ src/Calculator.php
@@ -0,0 +1,9 @@
+<?php
+
+class Calculator
+{
+    public function add(int $a, int $b)
+    {
+        return $a+$b;
+    }
+}
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+require __DIR__ . "/../src/Calculator.php";
 
 class CalculatorTest extends TestCase
 {
@@ -9,6 +10,7 @@ class CalculatorTest extends TestCase
         $sut = new Calculator();
 
         $this->assertEquals( 8, $sut->add( 5, 3) );
+        $this->assertEquals( 5, $sut->add( 5, 0) );
 
     }
 }
